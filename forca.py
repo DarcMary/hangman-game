1d97c993b1fcf3dd6fc5c308e0d6b3d872ec66b8
Darc Mary <darcmary12@gmail.com>
2025-02-27 00:35:09 -0300

    feat: implement replacement of dashes with correct letters in the hangman game

--- forca.py
+++ forca.py
@@ -1,24 +1,25 @@
-"""
- Desenvolver um jogo clássico da forca onde o jogador tenta adivinhar
-uma palavra oculta letra por letra.
- Pode incluir funcionalidades como seleção de categorias de palavras,
-gráficos simples para representar o progresso do jogo e contagem de
-tentativas.
- É um projeto que permite explorar estruturas de dados como listas e
-strings, além de laços de repetição e condicionais.
-"""
 import random
 
-palavras = ["computador", "programação", "software", "hardware", "internet", "inteligência artificial", "criptografia", "cibersegurança", "desenvolvimento", "algoritmo", "backend", "frontend", "javascript", "python", "html", "css", "react", "database", "servidor", "cloud", "rede", "dados", "bit", "byte", "blockchain", "criptomoeda"]
+palavras = ["computador", "programação", "software", "hardware", "internet", "inteligência artificial", "criptografia", "cibersegurança", "desenvolvimento", "algoritmo", "backend", "frontend", "javascript", "python", "react", "database", "servidor", "cloud", "rede", "dados", "bit", "byte", "blockchain", "criptomoeda"]
 
 palavra_aleatoria = random.choice(palavras)
 
 letras = list(palavra_aleatoria)
-print(letras)
+palavra_oculta = ["_" if letra != " " else " " for letra in palavra_aleatoria]
 
-letra_usuario = input("Digite uma letra para começar o jogo:").lower()
+print("Palavra: ", " ".join(palavra_oculta))
 
-if letra_usuario in letras:
-    print(f"A letra '{letra_usuario}' está na palavra!")
-else:
-    print(f"A letra '{letra_usuario}' não está na palavra.")
+while "_" in palavra_oculta:
+    letra_usuario = input("Digite uma letra:").lower()
+
+    if letra_usuario in letras:
+        print(f"A letra '{letra_usuario}' está na palavra!")
+        for i in range(len(letras)):
+            if letras[i] == letra_usuario:
+                palavra_oculta[i] = letra_usuario
+    else:
+        print(f"A letra '{letra_usuario}' não está na palavra.")
+
+    print("Palavra: ", " ".join(palavra_oculta))
+
+print("Parabéns, você descobriu a palavra!")
